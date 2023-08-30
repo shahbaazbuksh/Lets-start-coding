cf5c8edffefa1c18039a2a917e611e1c429735cd
shahbaazbuksh <48543913+shahbaazbuksh@users.noreply.github.com>
2023-08-30 09:19:06 +0530

    Lets start coding

---
+++ App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h2>Expense Items</h2>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
+    </div>
+  );
+}
+
+export default App;
