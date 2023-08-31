cf799352dc30348cd5e6897c0dcc41eda3502a05
shahbaazbuksh <48543913+shahbaazbuksh@users.noreply.github.com>
2023-08-31 10:20:00 +0530

    Added event listener

--- ExpenseItem.jsx
+++ ExpenseItem.jsx
@@ -5,12 +5,16 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickhandler = () => {
+    document.querySelector(".expense-item").innerHTML = "";
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickhandler}>Delete Expense</button>
       </div>
     </Card>
   );
