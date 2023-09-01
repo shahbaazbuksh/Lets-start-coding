80204c5063cf52db6db88a0dabe5356c57de97ff
shahbaazbuksh <48543913+shahbaazbuksh@users.noreply.github.com>
2023-09-01 10:01:16 +0530

    Added expense amount change state

--- ExpenseItem.jsx
+++ ExpenseItem.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [expense, setExpense] = useState(props.amount);
   const clickhandler = () => {
-    document.querySelector(".expense-item").innerHTML = "";
+    setTitle("Updated!!!");
   };
+  function changeExpense() {
+    setExpense(100);
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickhandler}>Delete Expense</button>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${expense}</div>
+        <button onClick={clickhandler}>Change Title</button>
+        <button onClick={changeExpense}>Change Expense</button>
       </div>
     </Card>
   );
